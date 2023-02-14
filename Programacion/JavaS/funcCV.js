3009234d6844eef58ab133c172507e01c0edcd0a
Pablo Herrera <paulybr2000@hotmail.com>
2023-02-13 23:26:16 -0300

    20230213

--- Programacion/JavaS/funcCV.js
+++ Programacion/JavaS/funcCV.js
@@ -17,7 +17,8 @@ $(document).ready(function(){
             if (titulo=="Mr" || titulo=="Monsieur"){titulo="Sr.";}
             else if (titulo=="Ms" || titulo=="Mrs" || titulo=="Madame"){titulo="Sra."}
             else if (titulo=="Miss" || titulo=="Mademoiselle"){titulo="Srta."}
-            let nombre=obj.results[0]['name']['first']+" "+obj.results[0]['name']['last'];
+            let nombre=obj.results[0]['name']['first'];
+            let apellido=obj.results[0]['name']['last'];
             let direccion=obj.results[0]['location']['street']['name']+" "+obj.results[0]['location']['street']['number'];
             let ciudad=obj.results[0]['location']['city']+"-  CP: "+obj.results[0]['location']['postcode'];
             let estado=obj.results[0]['location']['state']+" - "+obj.results[0]['location']['country'];
@@ -34,19 +35,46 @@ $(document).ready(function(){
             console.log(obj.results[0]['name']['title']);
             console.log(obj.results[0]['dob']['date']);
 
-            document.getElementById('nom').innerHTML=titulo+" "+nombre;
+            document.getElementById('nom').innerHTML=titulo+" "+nombre+" "+apellido;
+            document.getElementById('primernom').innerHTML=nombre;
+            document.getElementById('apell').innerHTML=apellido;
             document.getElementById('sex').innerHTML=sexo;
             document.getElementById('dir').innerHTML=direccion;
+            document.getElementById('dire').innerHTML=direccion;
             document.getElementById('city').innerHTML=ciudad;
+            document.getElementById('ciudad').innerHTML=ciudad;
             document.getElementById('estado').innerHTML=estado;
             document.getElementById('fecha').innerHTML=fechanac;
             document.getElementById('telef').innerHTML=phone;
+            document.getElementById('telefo').innerHTML=phone;
             document.getElementById('cellp').innerHTML=mobile;
             document.getElementById('fotos').innerHTML="<img src="+foto+" alt="+nombre+" style='width:50%'>";
             document.getElementById('mail').innerHTML="<a href='mailto:"+dirmail+"'>"+dirmail+"</a>";
+            document.getElementById('email').innerHTML="<a href='mailto:"+dirmail+"'>"+dirmail+"</a>";
+
+            document.getElementById("explabo").style.display='initial';
+            document.getElementById("estucurs").style.display='none';
+            document.getElementById("infoco").style.display='none';
 
         }
         });
     });
     
+})
+$(experiencia).click(function(){
+    document.getElementById("explabo").style.display='initial';
+    document.getElementById("estucurs").style.display='none';
+    document.getElementById("infoco").style.display='none';
+})
+
+$(estudios).click(function(){
+    document.getElementById("explabo").style.display='none';
+    document.getElementById("estucurs").style.display='initial';
+    document.getElementById("infoco").style.display='none';
+})
+
+$(infocomp).click(function(){
+    document.getElementById("explabo").style.display='none';
+    document.getElementById("estucurs").style.display='none';
+    document.getElementById("infoco").style.display='initial';
 })
